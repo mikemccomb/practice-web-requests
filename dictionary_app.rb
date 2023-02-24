020142acb38132796f368e840c12e7a94a8ae82e
mikemccomb <mikemccomb@gmail.com>
2023-02-24 14:00:29 -0600

    Does not break if word 404s

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -2,23 +2,31 @@ require "http"
 
 search = true
 while search
+  system "clear"
   puts "What word would you like to define?"
   word = gets.chomp
 
-  word_call = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=API_KEY").parse(:json)
+  word_call = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=ADD_KEY").parse(:json)
 
-  p word_call
-
-  puts "Here are the definitions I could find for '#{word}'"
-
-  word_call.each do |entry|
-    # puts "Part of speech: #{entry["partOfSpeech"]}"
-    if entry["text"].class == String
-      puts entry["text"]
-    end
-    puts ""
+  # pp word_call[0]
+  if word_call.class == Hash
+    puts "Hmmm, I couldn't find '#{word}'."
   end
 
+  if word_call.class == Array
+    puts "Here is the first definition I could find for '#{word}':"
+    puts ""
+    index = 0
+    while index < word_call.length
+      if word_call[index]["text"]
+        puts word_call[index]["text"]
+        index = word_call.length
+      else
+        index += 1
+      end
+    end
+  end
+  puts ""
   puts "Would you like to look up another word? (y/n)"
   continue = gets.chomp.downcase
   if continue == "n"
