173d94b6cee964d83c197cd720263281134eead4
mikemccomb <mikemccomb@gmail.com>
2023-03-03 11:59:44 -0600

    Update product test

--- product_test.rb
+++ product_test.rb
@@ -6,6 +6,10 @@ products = response.parse
 pp products
 puts ""
 puts "View one"
-response = HTTP.get("http://localhost:3000/products/2.json")
+response = HTTP.get("http://localhost:3000/products/5.json")
 product = response.parse
 pp product
+puts ""
+puts "Update name:"
+name = gets.chomp
+response = HTTP.get("http://localhost:3000/products/5.json#update")
